fa5657d9d825461ba30ded1cba015036ad165c73
Daniel de Albuquerque Rangel Moreira <danielrangelmoreira@gmail.com>
2013-02-20 23:37:27 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peth::Application.config.secret_token = 'f3534926a0067c49f5821fb965ea59e9e632a4bfb2d9777dfeab74c11e42828081ca3769b583d814495f6aa94418c1da2f5da9f4c092555193abc9d5e76286b3'
