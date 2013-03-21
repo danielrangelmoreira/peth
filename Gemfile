fc117629bbd28ae2d794e5ec57ddd2512a7b9bc0
Daniel de Albuquerque Rangel Moreira <danielrangelmoreira@gmail.com>
2013-03-20 21:26:38 -0300

    Initial commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 gem 'rails', '3.2.11'
 
@@ -22,7 +22,7 @@ group :assets do
   
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
@@ -52,3 +52,5 @@ end
 
 # To use debugger
 # gem 'debugger'
+gem 'rubber'
+gem 'open4'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,20 @@ Peth::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+ #Action::Mailer
+  config.action_mailer.default_url_options = { :host => 'ec2-50-19-79-61.compute-1.amazonaws.com' }
+# ActionMailer Config
+# Setup for production - deliveries, no errors raised
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_API_KEY"]
+}
+
 end
