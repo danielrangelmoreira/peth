878c91209c51f7d2ecd09c00e13b1d502fe74994
Daniel de Albuquerque Rangel Moreira <danielrangelmoreira@gmail.com>
2013-03-23 23:00:58 -0300

    fix email issues

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'devise'
 gem 'jquery-rails'
 gem 'pg_search'
 gem 'simple_form'
-  gem 'figaro'
+gem 'figaro'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
-  :user_name => ENV["MANDRILL_USERNAME"],
-  :password  => ENV["MANDRILL_API_KEY"]
+  :user_name => "danielrangelmoreira@gmail.com",
+  :password  => "MuWh06TLlGv778PRnnMdUA"
 }
 
 end
